4e7ab74ebfe8bd970f55773e6e1c9430847617e9
Flujos04 <44932923+Flujos04@users.noreply.github.com>
2018-11-11 02:46:10 +0100

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,4 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a=('Soler es un capullo que nos mata a trabajar')
+>>> print(a)
