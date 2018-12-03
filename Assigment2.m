4dbfd3592948204183730f1d6442369d45e3c2cf
Flujos04 <44932923+Flujos04@users.noreply.github.com>
2018-12-03 13:06:41 +0100

    Add files via upload

--- Assigment2.m
+++ Assigment2.m
@@ -6,9 +6,20 @@ Ny=100;
 dx=3*L/(Nx-1);
 x=0:dx:3*L;
 R=(5/4 + 0.05)*L;
+
 y0=zeros(1,Nx);
 y0(1:Nx/3)=0; %BOTTOM LINE HEIGHT EQUATION 
-y0((Nx/3 + dx):(Nx*2/3 - dx))=sqrt(R^2 - (x((Nx/3 + dx):(Nx*2/3 - dx))-1.5).^2) - (R - 0.1*L);%BOTTOM LINE HEIGHT EQUATION
-y0(Nx*3*0.5:Nx)=0;%BOTTOM LINE HEIGHT EQUATION
+y0((Nx/3 +1):(Nx*(2/3)))=sqrt(R^2 - (x((Nx/3 +1):(Nx*2/3))-1.5*L).^2) - (R - 0.1*L);%BOTTOM LINE HEIGHT EQUATION
+y0(Nx*(2/3):Nx)=0;%BOTTOM LINE HEIGHT EQUATION
+
 dy=(L-y0)/(Ny-1);
-y=0:dy:L;
+
+
+
+
+
+[xmesh,ymesh]=meshgrid(x,y);
+figure;
+surf(xmesh,ymesh,0);
+xlabel('x');
+ylabel('y');
