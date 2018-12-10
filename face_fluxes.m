86ae76924b7b2b4293162a7dde8e47733b70a4d1
carloscstm <45629749+carloscstm@users.noreply.github.com>
2018-12-10 13:04:44 +0100

    Create face_fluxes.m

---
+++ face_fluxes.m
@@ -0,0 +1,58 @@
+function [FN, FE, FS, FW] = face_fluxes(f,g,Nx_N,Ny_N,Nx_E,Ny_E,Nx_S,Ny_S,Nx_W,Ny_W,Ny,Nx)%D,
+
+
+%NORTH FACES
+FN = zeros((Ny-1),(Nx-1),4);
+    %Boundary
+        j=Ny-1;
+        FN(j,:,:)=f(j,:,:).*Nx_N(j,:)+g(j,:,:).*Ny_N(j,:);%-D()
+                
+    %Rest        
+        for i=1:(Nx-1)
+          for j=1:(Ny-2)
+               FN (j,i,:) =(1/2)*(f(j,i,:)+f(j+1,i,:))*Nx_N(j,i) + (1/2)*(g(j,i,:)+g(j+1,i,:))*Ny_N(j,i);%-D() 
+          end
+        end
+        
+        
+%EAST FACES
+FE = zeros((Ny-1),(Nx-1),4);
+    %Boundary
+        i=Nx-1;
+        FE(:,i,:)=f(:,i,:).*Nx_E(:,i)+g(:,i,:).*Ny_E(:,i);%-D()
+                
+    %Rest        
+        for i=1:(Nx-2)
+          for j=1:(Ny-1)
+               FE (j,i,:) =(1/2)*(f(j,i,:)+f(j,i+1,:))*Nx_E(j,i) + (1/2)*(g(j,i,:)+g(j,i+1,:))*Ny_E(j,i);%-D() 
+          end
+        end
+        
+        
+%SOUTH FACES
+FS = zeros((Ny-1),(Nx-1),4);
+    %Boundary
+        j=1;
+        FN(j,:,:)=f(j,:,:).*Nx_S(j,:)+g(j,:,:).*Ny_S(j,:);%-D()
+                
+    %Rest        
+        for i=1:(Nx-1)
+          for j=2:(Ny-1)
+               FS (j,i,:) =(1/2)*(f(j,i,:)+f(j-1,i,:))*Nx_S(j,i) + (1/2)*(g(j,i,:)+g(j-1,i,:))*Ny_S(j,i);%-D() 
+          end
+        end        
+ 
+        
+%WEST FACES
+FW = zeros((Ny-1),(Nx-1),4);
+    %Boundary
+        i=1;
+        FW(:,i,:)=f(:,i,:).*Nx_W(:,i)+g(:,i,:).*Ny_W(:,i);%-D()
+                
+    %Rest        
+        for i=2:(Nx-1)
+          for j=1:(Ny-1)
+               FW (j,i,:) =(1/2)*(f(j,i,:)+f(j,i-1,:))*Nx_W(j,i) + (1/2)*(g(j,i,:)+g(j,i-1,:))*Ny_W(j,i);%-D() 
+          end
+        end  
+end
