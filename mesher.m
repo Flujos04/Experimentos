9174c9b30e8d5a62b17b2df7a1a1dd9c387ada5b
Flujos04 <44932923+Flujos04@users.noreply.github.com>
2018-12-10 09:34:58 +0100

    Update mesher.m

--- mesher.m
+++ mesher.m
@@ -1,13 +1,25 @@
 
-function [x,y,y0]=mesher(L,Nx,Ny)
+function [x,y,y0,dx,dy]=mesher(L,Nx,Ny)
+
+
 dx=3*L/(Nx-1);
 x=0:dx:3*L;
 R=(1.3)*L;
-y0=zeros(1,Nx);
-xarc=L:dx:L*2;
 
-    yarc=sqrt(R^2 - (xarc-1.5*L).^2) - 1.2*L;%BOTTOM LINE HEIGHT EQUATION
-    y0=[(0:dx:L-dx)*0 yarc (0:dx:L-dx)*0];
+xinit=0:dx:L;
+xarc=L:dx:L*2;
+xfinal=2*L:dx:3*L;
+
+x(1:length(xinit))=xinit; 
+x(length(xinit):length(xinot)+length(xarc)-1)=xarc;
+x(length(xint)+length(xarc)-1:length(xinit)+length(xarc)+length(xfinal)-2)=xfinal;
+
+
+
+    lilbump=sqrt(R^2 - (xarc-1.5*L).^2) - 1.2*L;
+    y0(length(xinit):length(xinit)+length(xarc)-1)=lilbump;
+    y0(1:length(xinit))=0;
+    y0(length(xinit)+length(xarc)-1:length(xinit)+length(xarc)+length(xfinal)-2)=0;
     dy=(L-y0)./(Ny-1);
     y=zeros(Ny,Nx);
     
@@ -19,6 +31,6 @@ for i=1:Ny
     
    xmatrix(i,:)=x; 
 end
-figure;hold on;
+
 surf(xmatrix,y,xmatrix.*0);
 end
