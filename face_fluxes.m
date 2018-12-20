8ae4e5eb7c345fdf9b41d97f4b45c913ac78dedf
carloscstm <45629749+carloscstm@users.noreply.github.com>
2018-12-20 10:32:44 +0100

    Update face_fluxes.m

--- face_fluxes.m
+++ face_fluxes.m
@@ -1,6 +1,5 @@
 function [FN, FE, FS, FW] = face_fluxes(f,g,Nx_N,Ny_N,Nx_E,Ny_E,Nx_S,Ny_S,Nx_W,Ny_W,Ny,Nx,D_north,D_west,D_east,D_south)
-
-
+         
 %NORTH FACES
 FN = zeros((Ny-1),(Nx-1),4);
     %Boundary
@@ -10,7 +9,7 @@ FN = zeros((Ny-1),(Nx-1),4);
     %Rest        
         for i=1:(Nx-1)
           for j=1:(Ny-2)
-               FN (j,i,:) =(1/2)*(f(j,i,:)+f(j+1,i,:))*Nx_N(j,i) + (1/2)*(g(j,i,:)+g(j+1,i,:))*Ny_N(j,i)-D_north(j,i,:); 
+               FN (j,i,:) =(1/2)*(f(j,i,:)+f(j+1,i,:))*Nx_N(j,i) + (1/2)*(g(j,i,:)+g(j+1,i,:))*Ny_N(j,i)-D_north(j,i,:);
           end
         end
         
@@ -24,7 +23,7 @@ FE = zeros((Ny-1),(Nx-1),4);
     %Rest        
         for i=1:(Nx-2)
           for j=1:(Ny-1)
-               FE (j,i,:) =(1/2)*(f(j,i,:)+f(j,i+1,:))*Nx_E(j,i) + (1/2)*(g(j,i,:)+g(j,i+1,:))*Ny_E(j,i)-D_east(j,i,:); 
+               FE (j,i,:) =(1/2)*(f(j,i,:)+f(j,i+1,:))*Nx_E(j,i) + (1/2)*(g(j,i,:)+g(j,i+1,:))*Ny_E(j,i)-D_east(j,i,:);
           end
         end
         
@@ -38,7 +37,7 @@ FS = zeros((Ny-1),(Nx-1),4);
     %Rest        
         for i=1:(Nx-1)
           for j=2:(Ny-1)
-               FS (j,i,:) =(1/2)*(f(j,i,:)+f(j-1,i,:))*Nx_S(j,i) + (1/2)*(g(j,i,:)+g(j-1,i,:))*Ny_S(j,i)-D_south(j,i,:);
+               FS(j,i,:) =(1/2)*(f(j,i,:)+f(j-1,i,:))*Nx_S(j,i) + (1/2)*(g(j,i,:)+g(j-1,i,:))*Ny_S(j,i)-D_south(j,i,:);
           end
         end        
  
@@ -56,3 +55,4 @@ FW = zeros((Ny-1),(Nx-1),4);
           end
         end 
 end
+
